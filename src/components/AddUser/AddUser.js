cbb8f8bffdae3be711181b193080219b4fdc8903
David <dmontanez@aceproject.space>
2022-11-26 16:37:29 -0600

    Configured form submission values of username and age

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,20 +1,69 @@
+import { useState } from "react";
+
 /**
  * Returns the Add User Form Component.
  *
  * @returns The Add User Form Component.
  */
 const AddUser = () => {
+    const [age, setAge] = useState(0);
+    const [username, setUsername] = useState("");
+
+    /**
+     * Handles the onChange of username input.
+     * 
+     * @param {Form Input Handler Event} event 
+     */
+    const userAgeHandler = (event) => {
+        setAge(event.target.value);
+
+        // console.log(event.target.value, "AddUser.js");
+    } 
+
+    /**
+     * Handles the onChange of username input.
+     * 
+     * @param {Form Input Handler Event} event 
+     */
+    const userNameHandler = (event) => {
+        setUsername(event.target.value);
+
+        // console.log(event.target.value, "AddUser.js");
+    }
+
+    /**
+     * Handles the form submission of adding a new user.
+     * 
+     * @param {Form Submit Handler Event} event 
+     */
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        console.log(`Name: ${username} Age: ${age}`, "AddUser.js");
+    };
+
     return (
         <>
             <div>
-                <form>
+                <form onSubmit={submitHandler}>
                     <label>Username</label>
-                    <input type="text" />
+                    <input
+                        type="text"
+                        id="username"
+                        name="username"
+                        onChange={userNameHandler}
+                    />
 
                     <label>Age (Years)</label>
-                    <input type="number" />
+                    <input
+                        min="1"
+                        id="age"
+                        name="age"
+                        type="number"
+                        onChange={userAgeHandler}
+                    />
 
-                    <button>Add User</button>
+                    <button type="submit">Add User</button>
                 </form>
             </div>
         </>
